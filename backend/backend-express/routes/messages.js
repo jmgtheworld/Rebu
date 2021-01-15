8518961c9df8a6006bd49bd0f08d95c7551e98bc
Paulo Santiago <p.santiago796@gmail.com>
2021-01-15 21:42:14 +0000

    comment out messages post request

--- backend/backend-express/routes/messages.js
+++ backend/backend-express/routes/messages.js
@@ -13,15 +13,15 @@ module.exports = ({
             }));
     });
 
-    router.post('/:id', function (req, res) {
-      const { customer_id, driver_id, message } = req.body;
-      db.query (
-        `
-        INSERT INTO messages (customer_id, driver_id, message)
-        VALUES ($1, $2, $3::text)
-        `
-      )
-    }, [customer_id, driver_id, message]).then(() => res.json("message added!"));
+    // router.post('/:id', function (req, res) {
+    //   const { customer_id, driver_id, message } = req.body;
+    //   db.query (
+    //     `
+    //     INSERT INTO messages (customer_id, driver_id, message)
+    //     VALUES ($1, $2, $3::text)
+    //     `
+    //   )
+    // }, [customer_id, driver_id, message]).then(() => res.json("message added!"));
 
     return router;
 };
