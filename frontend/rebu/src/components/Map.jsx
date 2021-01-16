a677517ebbe7d9d024ed5ac733b8175387a5b039
jmgtheworld <avedj94@hotmail.com>
2021-01-15 19:33:20 -0500

    add maps api/component with marker

---
+++ frontend/rebu/src/components/Map.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { GoogleMap,  Marker, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '800px',
+  height: '800px'
+};
+
+const center = {
+  lat: 37.772,
+  lng: -122.214
+};
+
+export default function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyBKY8_MFv_adJoVEQDGNhzhucO-UnI2Lhs"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  const position = {
+    lat: 37.772,
+    lng: -122.214
+  }
+  
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <Marker
+          position={position}
+        />
+      </GoogleMap>
+  ) : <></>
+}
