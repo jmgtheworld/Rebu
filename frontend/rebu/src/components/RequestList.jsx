8b91f7ca372f3c06dabf4aa13d6b8ce6bcafcee8
Jenny <jenjung2194@gmail.com>
2021-01-17 14:23:59 -0500

    add settings that filter request by distance from the driver to the user

--- frontend/rebu/src/components/RequestList.jsx
+++ frontend/rebu/src/components/RequestList.jsx
@@ -1,5 +1,4 @@
-import { Card, Col, Row, Button } from 'react-bootstrap';
-import { AiFillSetting } from 'react-icons/ai';
+import { Card, Col, Row, Dropdown, DropdownButton, ButtonGroup} from 'react-bootstrap';
 
 import RequestCard from "./RequestCard";
 
@@ -26,10 +25,24 @@ export default function RequestList (props) {
           <Col column sm={3}>From</Col>
           <Col column sm={3}>To</Col>
           <Col column sm={2}>Price</Col>
-          <Col></Col>
-          <Button>
-            <AiFillSetting />
-          </Button>
+          <Col>
+            <DropdownButton
+              as={ButtonGroup}
+              menuAlign="right"
+              key="Secondary"
+              id="dropdown-variants-Secondary"
+              title="Settings"
+              variant="secondary"
+            >
+              Distance
+              <Dropdown.Divider />
+              <Dropdown.Item eventKey="1">500m</Dropdown.Item>
+              <Dropdown.Item eventKey="2">1km</Dropdown.Item>
+              <Dropdown.Item eventKey="3">5km</Dropdown.Item>
+              <Dropdown.Item eventKey="4">10km</Dropdown.Item>
+              <Dropdown.Item eventKey="4">20km</Dropdown.Item>
+            </DropdownButton>
+          </Col>
         </Row>
       </Card.Header>
       {requests}
