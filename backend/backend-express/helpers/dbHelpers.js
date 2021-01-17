505d2d65a8e1bd76dd11f5dbbc93cab75c317f0b
Paulo Santiago <p.santiago796@gmail.com>
2021-01-17 22:14:14 +0000

    add post request to user to update their current location

--- backend/backend-express/helpers/dbHelpers.js
+++ backend/backend-express/helpers/dbHelpers.js
@@ -1,3 +1,5 @@
+const request = require('request-promise-native');
+
 module.exports = (db) => {
     // Gets all users in DB
     const getUsers = () => {
@@ -24,6 +26,7 @@ module.exports = (db) => {
             .catch((err) => err);
     };
 
+    // Get all trips from a user
     const getTripsByUserId = id => {
         const query = {
             text: `SELECT * FROM trips WHERE customer_id = $1`,
@@ -36,18 +39,39 @@ module.exports = (db) => {
             .catch((err) => err);
     };
 
-    // const getUserCurrentLocation = id => {
+    // Get user's IP address
+    const IPurl = 'https://api.ipify.org?format=json';
 
-    // };
+    const fetchIP = () => {
+        return request(IPurl);
+    };
+
+    // Convert user's IP to coordinates
+    const fetchCoordsByIP = (body) => {
+        const ip = JSON.parse(body).ip;
+        return request(`http://ip-api.com/json/${ip}`);
+      };
+
+    // Update the user's current location (lat and lon)
+    const updateUserCurrentLocation = (lat, lon, id) => {
+        const query = {
+            text: `UPDATE users SET current_location_lat = $1, current_location_lon = $2 WHERE id = $3`,
+            values: [lat, lon, id]
+        };
+
+        return db.query(query)
+            .then(result => result.rows[0])
+            .catch(err => err);
+    };
 
     // Add new user to DB
-    const addUser = (driver, full_name, email, created_at, phone_number, credit_card, month_year, cvc, license, street_address, apartment_number, city, postal_code, province, country, password) => {
+    const addUser = (driver, full_name, email, created_at, phone_number, credit_card, month_year, cvc, license, street_address, apartment_number, city, postal_code, province, country, current_location_lat, current_location_lon, password) => {
         const query = {
             text: `
-                INSERT INTO users (driver, full_name, email, created_at, phone_number, credit_card, month_year, cvc, license, street_address, apartment_number, city, postal_code, province, country, password)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
+                INSERT INTO users (driver, full_name, email, created_at, phone_number, credit_card, month_year, cvc, license, street_address, apartment_number, city, postal_code, province, country, current_location_lat, current_location_lon, password)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)
             `,
-            values: [driver, full_name, email, created_at, phone_number, credit_card, month_year, cvc, license, street_address, apartment_number, city, postal_code, province, country, password]
+            values: [driver, full_name, email, created_at, phone_number, credit_card, month_year, cvc, license, street_address, apartment_number, city, postal_code, province, country, current_location_lat, current_location_lon, password]
         };
   
         return db.query(query)
@@ -66,7 +90,7 @@ module.exports = (db) => {
             .query(query)
             .then(result => result.rows[0])
             .catch((err) => err);
-    }
+    };
   
     // Gets all trips in DB
     const getTrips = () => {
@@ -158,6 +182,9 @@ module.exports = (db) => {
         addTrip,
         getMessages,
         getMessageById,
-        getTripsByUserId
+        getTripsByUserId,
+        updateUserCurrentLocation,
+        fetchIP,
+        fetchCoordsByIP
     };
   };
