a427f7d3c38362fd37b64147e6bf172502205fb5
Jenny <jenjung2194@gmail.com>
2021-01-21 14:53:19 -0500

    merged HomeDriver.jsx with HomeDriverWithMap.jsx

--- frontend/rebu/src/components/HomeDriver.jsx
+++ frontend/rebu/src/components/HomeDriver.jsx
@@ -11,10 +11,35 @@ import './Chat/Chat.css'
 export default function HomeDriver (props) {
   const [requests, setRequests] = useState([]); 
 
+  const[travelMode, setTravelMode] = React.useState("WALKING");
+
+  const [driverlocation, setDriverlocation] = useState([]); 
+
   const [travelTD, settravelTD] = useState({
     time: "0",
     distance: "0",
-  }) 
+  })
+
+  const [price, setPrice] = useState("");
+
+  const [startAddress, setstartAddress] = useState("");
+  const [finishAddress, setfinishAddress] = useState("");
+  
+  // Coords
+  const[origin, setOrigin] = useState({
+    lat: null, 
+    lng: null 
+  });
+
+  const [pickup, setPickup] = useState({
+    lat: null, 
+    lng: null
+  });
+  
+  const[destination, setDestination] = useState({
+    lat: null, 
+    lng: null
+  }); 
   
   const requestsAPI = "http://localhost:3001/trips/not-accepted";
 
@@ -23,33 +48,71 @@ export default function HomeDriver (props) {
       .then(res => setRequests(res.data));
   },[requestsAPI])
 
-
-  //function to filter requests by distances
-  //function when the request is accepted
-    //put requests to api/trips/:id and changes accepted column to 'true' and input the user's id into the driver_id column
-    //and directs the user to the /trip view route where the navigation starts
+  useEffect(() => {
+    const requestsAPI = "http://localhost:3001/users"
+    Axios.get(requestsAPI) //would be /api/trips/requested to get trips that have the accepted===false
+      .then(res => setDriverlocation(res.data));
+  },[])
 
     
   return (
     <div>
       <div>Map to show other drivers</div>
-      <div className = "map">
-        <MapDriver travelTD = {travelTD} settravelTD = {settravelTD}/>
-      </div>
-      <DriverSummary travelTD = {travelTD} settravelTD = {settravelTD}/>
-      <div id="chat">
-        <h2>Chat</h2>
-        <Chat
-          name={props.user.full_name}
-          driver={props.user.driver}
-        />
-      </div>
       <div>
         <h2>Requests</h2>
+        <div className = "map">
+          <MapDriver 
+            driverlocation = {driverlocation[1]}
+            travelMode = {travelMode}
+            setTravelMode = {setTravelMode}
+            travelTD = {travelTD} settravelTD = {settravelTD}
+            origin = {origin} setOrigin = {setOrigin} 
+            pickup = {pickup}
+            destination = {destination} setDestination = {setDestination}
+            startAddress = {startAddress} setstartAddress = {setstartAddress}
+            finishAddress = {finishAddress} setfinishAddress = {setfinishAddress}
+          />
+        </div>
+        <DriverSummary price = {price} travelMode = {travelMode} travelTD = {travelTD} settravelTD = {settravelTD}/>
+        <div id="chat">
+          <h2>Chat</h2>
+          <Chat
+            name={props.user.full_name}
+            driver={props.user.driver}
+          />
+      </div>
         <RequestList 
-          requests={requests} 
+          driverlocation = {driverlocation[1]}
+          requests = {requests} 
+          origin = {origin}
+          pickup = {pickup}
+          destination = {destination}
+          setOrigin = {setOrigin} 
+          setPickup = {setPickup}
+          setDestination = {setDestination}
+          setPrice = {setPrice}
         />
       </div>
     </div>
+    // <div>
+    //   <div>Map to show other drivers</div>
+    //   <div className = "map">
+    //     <MapDriver travelTD = {travelTD} settravelTD = {settravelTD}/>
+    //   </div>
+    //   <DriverSummary travelTD = {travelTD} settravelTD = {settravelTD}/>
+    //   <div id="chat">
+    //     <h2>Chat</h2>
+    //     <Chat
+    //       name={props.user.full_name}
+    //       driver={props.user.driver}
+    //     />
+    //   </div>
+    //   <div>
+    //     <h2>Requests</h2>
+    //     <RequestList 
+    //       requests={requests} 
+    //     />
+    //   </div>
+    // </div>
   )
 }
