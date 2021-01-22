018c62a13be695cef05524b5ba85a75f87bd0e4e
jmgtheworld <avedj94@hotmail.com>
2021-01-21 21:31:51 -0500

    set accpeted to true once driver accepts trip request

--- frontend/rebu/src/components/ConfirmModal.jsx
+++ frontend/rebu/src/components/ConfirmModal.jsx
@@ -5,6 +5,7 @@ import {Modal, Button} from 'react-bootstrap';
 export default function ConfirmModal (props) {
 
   const {confirm, driverlocation, origin, setOrigin, destination, 
+    start_address, end_address, price,
     setDestination, pickup, setPickup, setPrice, start_location_lat, start_location_lon,
     end_location_lat, end_location_lon
   } = props;
@@ -14,10 +15,40 @@ export default function ConfirmModal (props) {
   console.log('start location lat' , start_location_lat)
   console.log('start location lon' , start_location_lon)
 
+  const controlDestinationLat = () => {
+    if ( end_location_lat){
+      return  end_location_lat
+    }
+    return null
+  }
+
+  const controlDestinationLng = () => {
+    if (end_location_lon){
+      return end_location_lon
+    }
+    return null
+  }
+
+  const [ trip, setTrip ] = useState({
+    customer_id: 1,
+    driver_id: null,
+    start_address: end_address,
+    end_address: start_address,
+    start_location_lat: controlDestinationLat(),
+    start_location_lon: controlDestinationLng(),
+    end_location_lat: start_location_lat,
+    end_location_lon: start_location_lon,
+    accepted: true,
+    payment_amount: price,
+    payment_status: false,
+    created_at: Date.now(),
+    ended_at: null
+  })
+
   useEffect(() => {
     if (loaded) {
-      console.log('newtrip', )
-      return Axios.put("http://localhost:3001/trips/1/accept", )
+      console.log('newtrip', trip)
+      return Axios.put("http://localhost:3001/trips/1/accept", trip)
         .then(() => console.log("new trip request created"))
         .catch(err => console.log(err));
     }
@@ -25,22 +56,23 @@ export default function ConfirmModal (props) {
 
 
   const confirmTrip = () => {
-    // setloaded(true)
-      setOrigin({
-        lat: driverlocation.current_location_lat,
-        lng: driverlocation.current_location_lon
-      })
-      setPickup({
-        lat: start_location_lat,
-        lng: start_location_lon
-      })
-      setDestination({
-        lat: end_location_lat, 
-        lng: end_location_lon
-      })
-      console.log('origin after submit', origin)
-      console.log('pickup after submit', pickup)
-      console.log('destination after submit', destination)
+    setloaded(true)
+    setTrip({
+      customer_id: 1,
+      driver_id: null,
+      start_address: start_address,
+      end_address: end_address,
+      start_location_lat: origin.lat,
+      start_location_lon: origin.lng,
+      end_location_lat: controlDestinationLat(),
+      end_location_lon: controlDestinationLng(),
+      accepted: true,
+      payment_amount: price,
+      payment_status: false,
+      created_at: Date.now(),
+      ended_at: null
+    })
+    console.log('trip confirmed', trip)
   }
 
   return (
