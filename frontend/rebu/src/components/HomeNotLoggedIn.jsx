bc520417071c0bb4861ee71c6d7c38a3241ebdce
Paulo Santiago <p.santiago796@gmail.com>
2021-01-24 00:04:57 +0000

    add fonts

--- frontend/rebu/src/components/HomeNotLoggedIn.jsx
+++ frontend/rebu/src/components/HomeNotLoggedIn.jsx
@@ -23,14 +23,12 @@ const HomeNotLoggedIn = () => {
 
   return (
     <div id="body">
-      {/* <h1>Not logged in (adding stuff to this rn)</h1> */}
       <Trail open={open} onClick={() => set((state) => !state)}>
         <span id="app-name">Rebu</span>
         <span className="slogan">designated</span>
         <span className="slogan">driving</span>
         <span className="slogan">service</span>
       </Trail>
-      {/* <h2>test</h2> */}
       <div className="button-group">
         <Button variant="success" size="lg" onClick={login} className = "RLbutton">
             Login
@@ -43,7 +41,5 @@ const HomeNotLoggedIn = () => {
     </div>
   )
 }
-
-// render(<HomeNotLoggenIn />, document.getElementById('root'))
 
 export default HomeNotLoggedIn;
